e143724b2198a24e04371134515104995490d06d
Chen-Steve <zhangc2@illinois.edu>
2026-03-02 19:42:34 -0600

    move around stuff add counter text

--- MP2/Assets/Scripts/ResourceBank.cs
+++ MP2/Assets/Scripts/ResourceBank.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class ResourceBank : MonoBehaviour
 {
     public float money;
+    public TextMeshProUGUI moneyText;
+
+    void Start()
+    {
+        UpdateUI();
+    }
 
     public void AddMoney(float amount)
     {
         money += amount;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (moneyText != null)
+            moneyText.text = "$ " + Mathf.FloorToInt(money).ToString();
     }
 }
